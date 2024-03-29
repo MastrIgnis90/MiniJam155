c3ec2962051e57b86322ac2faa61dd525b1f0155
Sebastian Wild <Mastryi90@gmail.com>
2024-03-29 00:05:30 -0600

    Adding git source control plugin

---
+++ MiniJam155Game/Source/MiniJam155Editor/Private/MiniJam155Editor.cpp
@@ -0,0 +1,17 @@
+﻿#include "MiniJam155Editor.h"
+
+#define LOCTEXT_NAMESPACE "FMiniJam155EditorModule"
+
+void FMiniJam155EditorModule::StartupModule()
+{
+    
+}
+
+void FMiniJam155EditorModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FMiniJam155EditorModule, MiniJam155Editor)
---
+++ MiniJam155Game/Source/MiniJam155Editor/Public/MiniJam155Editor.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Modules/ModuleManager.h"
+
+class FMiniJam155EditorModule : public IModuleInterface
+{
+public:
+    virtual void StartupModule() override;
+    virtual void ShutdownModule() override;
+};
